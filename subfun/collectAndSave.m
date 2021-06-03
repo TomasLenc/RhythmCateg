e9becf8ac8a5bd3e2b35db4bd5ea1742766bef54
Ceren <battal.ceren@gmail.com>
2021-06-03 13:39:28 +0200

    add NA for extracolumns
    commented now because it did not help.

--- subfun/collectAndSave.m
+++ subfun/collectAndSave.m
@@ -8,11 +8,30 @@ function responseEvents = collectAndSave(cfg, logFile, experimentStart)
         for iResp = 1:size(responseEvents, 1)
             responseEvents(iResp).onset = ...
                 responseEvents(iResp).onset - experimentStart;
+%             
+%             responseEvents(iResp).sequenceNum = 'n/a';
+%             responseEvents(iResp).segmentNum = 'n/a';
+%             responseEvents(iResp).segmentOnset = 'n/a';
+%             responseEvents(iResp).stepNum = 'n/a';
+%             responseEvents(iResp).stepOnset = 'n/a';
+%             responseEvents(iResp).patternID = 'n/a';
+%             responseEvents(iResp).segmentCateg = 'n/a';
+%             responseEvents(iResp).F0 = 'n/a';
+%             responseEvents(iResp).isTask = 'n/a';
+%             responseEvents(iResp).gridIOI = 'n/a';
+%             responseEvents(iResp).patternAmp = 'n/a';
+%             responseEvents(iResp).minPE4 = 'n/a';
+%             responseEvents(iResp).rangePE4 = 'n/a';
+%             responseEvents(iResp).minLHL24 = 'n/a';
+%             responseEvents(iResp).rangeLHL24 = 'n/a';
+%             responseEvents(iResp).LHL24 = 'n/a';
+%             responseEvents(iResp).PE4 = 'n/a';
+            responseEvents(iResp).target = sum(target);
+
         end
 
         responseEvents(1).fileID = logFile.fileID;
         responseEvents(1).extraColumns = logFile.extraColumns;
-        responseEvents(1).target = sum(target);
         
         responseEvents(1).isStim = logFile.isStim;
         
